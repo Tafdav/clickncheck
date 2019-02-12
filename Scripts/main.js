2bb773fe0d6b587e7370cd6cba0a1262b0eeaaf8
Tafdav <46773345+Tafdav@users.noreply.github.com>
2019-02-12 18:26:12 +0200

    added other members pages

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,10 @@
+const URL = 'http://localhost/api/DummyModels';
+
+
 function editProfileViewModel(){
 	var self = this;
-	self.firstName = ko.observable("Bert");
-	self.lastName = ko.observable("Bertington");
+	self.firstName = ko.observable();
+	self.lastName = ko.observable();
 	self.phone = ko.observable();
 	self.email = ko.observable();
 	self.password = ko.observable();
@@ -72,16 +75,36 @@ function editProfileViewModel(){
 		}
 		return canRegister;
 	};
-
+    /*
+    self.recruiter = ko.computed(new function(){
+        var rec = this;
+        rec.firstName = self.firstName;
+        rec.lastName = self.lastName;
+        rec.phone = self.phone;
+        rec.email = self.email;
+        rec.password = self.password;
+        rec.manager = self.manager;
+        return this;
+    });*/
+    //console.log(ko.toJSON(self.recruiter));
 	self.register = function(){
 		if(self.canRegister()){
-			console.log(ko.toJSON(this));
+            var body = {
+                firstName: self.firstName(),
+                LastName: self.lastName(),
+                Username: self.email(),
+                Password: self.password()
+            }
+            console.log(ko.toJSON(body));
+            
 			$.ajax({
-				url: "/api/recruiter/register",
-				type: "post",
-				data: ko.toJSON(this),
+                type: "post",
+                url: URL+"/users/register",
+                data: ko.toJSON(body),
+                dataType: "json",
 				contentType: "application/json",
 				success: function (result) {
+                    console.log(result);
 					//assuming result will contain the server-side customer id
 					//we provide that value to our customer's id observable
 					//and knockout will update the UI
