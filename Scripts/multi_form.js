6754c5541b9c36549e544e7aeced3a1da86d7f1c
flekhulani <flekhulani@retrorabbit.co.za>
2019-02-13 08:37:51 +0200

    Added backward transition between form completion

--- Scripts/multi_form.js
+++ Scripts/multi_form.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
        $('.current').removeClass('current').hide().next().show().addClass('current');
         $('#progress_bar li.active').next().addClass('active');
     });
+
+
+    $('#prev').click(function(){
+        $('.current').removeClass('current').hide().prev().show().addClass('current');
+        $('#progress_bar li.active').removeClass('active').prev().addClass('active');
+    });
 });
