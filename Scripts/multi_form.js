93efb5d4866ba1440fd78629fc597412115dfbbc
flekhulani <flekhulani@retrorabbit.co.za>
2019-02-13 09:05:35 +0200

    Hid the "prev" button when on the first step

--- Scripts/multi_form.js
+++ Scripts/multi_form.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
-    $('#button').click(function () {
+
+    if($('.field1.current')[0]){
+        $('#prev').hide();
+    }
+
+    $('#next').click(function () {
        $('.current').removeClass('current').hide().next().show().addClass('current');
-        $('#progress_bar li.active').next().addClass('active');
+        $('#progress_bar li.active').next().addClass('active'); 
+        $('#prev').show();
     });
+
 
 
     $('#prev').click(function(){
         $('.current').removeClass('current').hide().prev().show().addClass('current');
         $('#progress_bar li.active').removeClass('active').prev().addClass('active');
+        if($('.field1.current')[0]){
+            $('#prev').hide();
+        }
     });
 });
